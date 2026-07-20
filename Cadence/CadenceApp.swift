2f9f9f59ebd7b77fe3a66463b2195eb5490b17c9
Dhiraj1604 <dhirajbodake16@gmail.com>
2026-07-20 13:27:14 +0530

    Initial Commit

---
+++ Cadence/CadenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadenceApp.swift
+//  Cadence
+//
+//  Created by Dhiraj on 20/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CadenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
